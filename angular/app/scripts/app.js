7e74a4eef5d3b5e9f8b7d495714f1dabf4de3d9a
David Jimenez-Morales <biodavidjm@gmail.com>
2014-12-01 19:16:11 -0600

    front-image directive for the frontpage
    * An angular carousel directive

--- angular/app/scripts/app.js
+++ angular/app/scripts/app.js
@@ -11,6 +11,7 @@
 angular
   .module('dictyFrontpageApp', [
     'ngRoute',
+    'ngTouch',
     'dictyHeaderFooterApp',
     'frontImageApp'
   ])
--- angular/app/scripts/frontImage/appFrontImage.js
+++ angular/app/scripts/frontImage/appFrontImage.js
@@ -1,23 +1,26 @@
 'use strict';
 
 /**
- * @ngdoc directive 
- * @name dictyFooterApp
+ * @ngdoc module 
+ * @name frontImageApp
  * @description
  * directive of the main image of the dicty front page
  */
 
 angular
-  .module('frontImageApp', [])
+  .module('frontImageApp', ['ui.bootstrap'])
   .directive('frontImage', function() {
     return{
       restrict:'E',
       templateUrl:'scripts/frontImage/front-image.html',
       scope: true,
       controller: function($scope) {
-        $scope.name = 'Frontpage Main Image';
+        $scope.name = 'Dicty Carousel Directive';
+        $scope.myInterval = 8000;
+        $scope.slides = [
+            {image: 'images/dictyPic.png', description: 'Whatever'},
+            {image: 'images/dev.jpg', description: 'Whatever'}
+        ];
       }
     };
   });
-
-  
