8dea1f682bd5acb5b924d9f783c70ce273dac6c4
David Jimenez-Morales <biodavidjm@gmail.com>
2014-12-18 14:24:16 -0600

    front-stockcenter directive
    The directive showing the new products at the front page

---
+++ app/scripts/frontStockCenter/appFrontStockCenter.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc module directive
+ * @name frontStockCenterApp.directive:frontStockCenter
+ * @description
+ * # Directive for the Stock Center shortcut shown in
+ * the front page
+ */
+
+angular
+  .module('frontStockCenterApp', [])
+    .directive('frontStockcenter', function() {
+      return{
+        restrict:'E',
+        templateUrl:'scripts/frontStockCenter/front-stockcenter.html',
+        scope: true,
+        controller: function($scope) {
+          $scope.stockcenterHeader = 'New in STOCK CENTER';
+        }
+      };
+    });
+
