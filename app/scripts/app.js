4261ed42d67e539b7a127340e11903640fb46e2a
David Jimenez-Morales <biodavidjm@gmail.com>
2015-06-24 15:43:04 -0500

    Make anchorScroll controller available to any page in the app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,6 +25,30 @@ angular
     'frontMeetingsApp',
     'definitionBarApp'
   ])
+  .run(['$anchorScroll', function($anchorScroll) {
+    $anchorScroll.yOffset = 50;   // always scroll by 50 extra pixels
+  }])
+  .controller('headerCtrl', ['$anchorScroll', '$location', '$scope', function ($anchorScroll, $location, $scope) 
+  {
+    $scope.gotoAnchor = function(x) 
+    {
+        // var newHash = 'anchor' + x;
+        var newHash = x;
+        if ($location.hash() !== newHash) 
+        {
+          // set the $location.hash to `newHash` and
+          // $anchorScroll will automatically scroll to it
+          // $location.hash('anchor' + x);
+          $location.hash(x);
+        } 
+        else 
+        {
+          // call $anchorScroll() explicitly,
+          // since $location.hash hasn't changed
+          $anchorScroll();
+        }
+    };
+  }])
   // .config(function ($routeProvider, $locationProvider) {
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/controllerViewsSections.js
+++ app/scripts/controllers/controllerViewsSections.js
@@ -10,29 +10,4 @@
 angular.module('dictyFrontpageApp')
 	.controller('researchViewCtrl', function ($scope) {
     	$scope.mainResearch = 'Research';
-  	})
-	.run(['$anchorScroll', function($anchorScroll) {
-		$anchorScroll.yOffset = 50;   // always scroll by 50 extra pixels
-	}])
-	.controller('headerCtrl', ['$anchorScroll', '$location', '$scope', function ($anchorScroll, $location, $scope) 
-	{
-		$scope.gotoAnchor = function(x) 
-		{
-		  	// var newHash = 'anchor' + x;
-		  	var newHash = x;
-		  	if ($location.hash() !== newHash) 
-		  	{
-		    	// set the $location.hash to `newHash` and
-		   		// $anchorScroll will automatically scroll to it
-		    	// $location.hash('anchor' + x);
-		    	$location.hash(x);
-		  	} 
-		  	else 
-		  	{
-			    // call $anchorScroll() explicitly,
-			    // since $location.hash hasn't changed
-			    $anchorScroll();
-		  	}
-		};
-	}
-]);
+  	});
